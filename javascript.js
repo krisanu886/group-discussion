5d278803de3c640eb24acd56b8fac4aab097783e
Krisanu <kisanudey886@gmail.com>
2021-12-23 18:25:28 +0530

    js discussion-Iterator

--- javascript.js
+++ javascript.js
@@ -1,6 +1,27 @@
 // Iterator discussion
 
+// creating Array------
+const myArray=["Sidharth","Poonam","Manjari","Kanhaiya"];
 
+//1. simple --Symbol.iterator()-- method use-----
+let name = myArray[Symbol.iterator]();
+console.log(name.next());
+console.log(name.next());
+console.log(name.next());
+console.log(name.next());
+console.log(name.next());
+
+// 2. for of loop & using Symbol.iteratormethod(Iterate Through Iterables)------
+for(items of myArray[Symbol.iterator]()){
+    document.write(items);
+}
+
+// 3. simple for of loop 
+for(items of myArray){
+    document.write(items);
+}
+
+//4. creating function-------
 function nameIterator(values){
     let nameIndex = 0;
     return{
@@ -19,8 +40,6 @@ function nameIterator(values){
         }
     }
 }
-const myArray=["Sidharth","Poonam","Manjari","Kanhaiya"];
-console.log(myArray);
 
 const name = nameIterator(myArray);
 
@@ -30,8 +49,6 @@ console.log(name.next().value);
 console.log(name.next().value);
 console.log(name.next().value);
 
-for(items of myArray){
-    document.write(items);
-}
+
 
 
